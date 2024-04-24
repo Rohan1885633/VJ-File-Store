6608cc5ff0fd7ccc31223b4e71399bb8d1356ed7
Rohan1885633 <141541061+Rohan1885633@users.noreply.github.com>
2024-04-24 10:55:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -47,7 +47,7 @@ DB_NAME = environ.get("DB_NAME", "vjbotz")
 # Bot Information
 BOT_TOKEN = environ.get("BOT_TOKEN", "6568240316:AAFBIdN5tEMOT8eZ7mJfdfjMjtQwecSi58k")
 BOT_USERNAME = environ.get("BOT_USERNAME", "The_Movies1_bot") # your bot username without @
-PICS = (environ.get('PICS', 'https://graph.org/file/82ef767ffebe3a948e476.jpg https://graph.org/file/82ef767ffebe3a948e476.jpg')).split() # Bot Start Picture
+PICS = (environ.get('PICS', 'https://graph.org/file/0e044bd7c1cecd7696dbd.jpg https://graph.org/file/0e044bd7c1cecd7696dbd.jpg')).split() # Bot Start Picture
 
 # Auto Delete Information
 AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
